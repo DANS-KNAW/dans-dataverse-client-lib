f5c8082cd81caf9ea138cdd01e8f1d574d68b4a9
Jan van Mansum <janvanmansum@gmail.com>
2026-02-16 16:45:15 +0100

    javadocs

--- lib/src/main/java/nl/knaw/dans/lib/dataverse/QueryContext.java
+++ lib/src/main/java/nl/knaw/dans/lib/dataverse/QueryContext.java
@@ -56,9 +56,9 @@ public final class QueryContext<T> implements AutoCloseable {
      * after processing each set of parameters. Note that types of parameters in the parameter sets must be compatible with the SQL query.
      *
      * @param sets   an iterable of parameter sets, where each parameter set is an array of objects corresponding to the parameters in the SQL query
-     * @param mapper a function that maps a ResultSet row to an object of type T
+     * @param mapper a function that maps a ResultSet row to an object of type U
      * @param <U>    the type of objects to return in the list
-     * @return a list of objects of type T resulting from executing the query for each set of parameters
+     * @return a list of objects of type U resulting from executing the query for each set of parameters
      * @throws SQLException when database access fails
      */
     public <U> List<U> executeFor(Iterable<Object[]> sets, Function<ResultSet, U> mapper) throws SQLException {
